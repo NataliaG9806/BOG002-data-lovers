f2fcaf866f458a58726507c7e64e0810b7cd7c7a
NataliaG9806 <garavitobarretonatalia@gmail.com>
2021-03-17 17:15:33 -0500

    pagina home, footer, ocultar pagina home

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { example } from './data.js';
+/*import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(example, data);*/
+
+paginas ();
+
+function paginas(){
+    const buttonCharacters = document.getElementById("buttonCharacters");
+    buttonCharacters.addEventListener('click', function(){
+        document.getElementById("homePage").style.visibility = "hidden";
+    }, false)
+
+    const buttonHome = document.getElementById("buttonHome");
+    buttonHome.addEventListener('click', function(){
+        document.getElementById("homePage").style.visibility = "visible";
+    }, false)
+
+    const buttonSeasons = document.getElementById("buttonSeasons");
+    buttonSeasons.addEventListener('click', function(){
+        document.getElementById("homePage").style.visibility = "hidden";
+    }, false)
+}
