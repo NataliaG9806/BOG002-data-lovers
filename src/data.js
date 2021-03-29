ee87e943a06b4564304675b348e513c18997823e
NataliaG9806 <garavitobarretonatalia@gmail.com>
2021-03-29 14:38:40 -0500

    calculo, pop up, ordenar A-Z / Z-A

--- src/data.js
+++ src/data.js
@@ -1,9 +1,47 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const filterData = (dataSearch, Specie) => {
+  if (Specie === "Species") {
+    return dataSearch;
+  } else {
+    dataSearch = dataSearch.filter((Character) => Character.species === Specie)
+  return dataSearch;
+  }
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const sortData = (dataSearch, way) => {
+  let dataLetter=[];
+  let NewArray=[];
+  const letras=["A","B","C","D","E","F","G","H","I","J","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  letras.map((item)=>   
+    dataLetter.push(dataSearch.filter((Characters) => Characters.name.split("")[0] === item)))
+    //console.log(dataLetter)
+  dataLetter.map((item)=> 
+    item.map((especific)=>
+    NewArray.push(especific)));
+    if(way === -1){
+      NewArray = NewArray.reverse(); 
+    }
+  return NewArray;
 };
+
+export const computeStats = (dataSearch) => {
+    let NewArray = [];
+    dataSearch.map((item) => NewArray.push(item.status));    
+    
+    let valueCalculate = NewArray.reduce((count, status) => { 
+    count[status] = (count[status] || 0) + 1;
+    return count;
+  }, {});
+
+  valueCalculate.Alive > 0 ? 
+  (valueCalculate.Alive = ((valueCalculate.Alive*100)/(dataSearch.length)).toFixed(2)) : (valueCalculate.Alive = 0);
+
+  valueCalculate.Dead > 0 ? 
+  (valueCalculate.Dead = ((valueCalculate.Dead*100)/(dataSearch.length)).toFixed(2)) : (valueCalculate.Dead = 0);
+
+  valueCalculate.unknown > 0 ?
+  (valueCalculate.unknown = ((valueCalculate.unknown*100)/(dataSearch.length)).toFixed(2)) : (valueCalculate.unknown = 0);
+
+  return valueCalculate;
+}
--- src/main.js
+++ src/main.js
@@ -1,35 +1,140 @@
-//import { example } from './data.js';
+import {  filterData, sortData, computeStats} from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
-let start = 0;
-let end = 50;
+
+//variables globales
+let start;
+let end;
 let dataSearch;
 let cross;
+const container = document.querySelector('.container');
 
-const rickandmorty = async () => {
-  const container = document.querySelector('.container');
-  container.innerHTML = data.results.map((item) => `
-        <div class="card">
-            <button class="cardImage">
-                <img src="${item.image}" />
-            </button>
-            <h1 class="nameCard">${item.name}</h1>
-        </div>
-        `).join("");
-};
-rickandmorty()
-window.onbeforeunload = completo();
+window.onload = complet(); //cada vez que se recargue la pagina
 
-document.getElementById("buttonCharacters").addEventListener("click", completo)
-
-function completo() {
-  dataSearch = data.results;
+function complet() {
+  dataSearch = data.results; //llama data completa
   start = 0;
   end = 50;
-  rickandmorty()
   page()
-  //console.log("entro recarga")
 }
 
+//funcion de paginacion
+function page() {
+  //console.log("entra page");
+  let dataSlice = dataSearch;
+  container.innerHTML = dataSlice.slice(start, end).map((item) => `
+  <div class="card">
+  <button class="cardImage">
+      <img src="${item.image}" />
+      <h1 class="nameCard">${item.name}</h1>
+  </button>        
+</div>
+    `).join("");
+  //console.log(start, end);
+  if (cross === 1) {
+    window.scrollTo(0, 0);
+  }
+  moreInfo()
+}
+
+//funcion mirar info especifica de cada personaje
+function moreInfo() {
+  const buttonsCharacter = document.querySelectorAll(".cardImage");
+  const wrapper = document.querySelector(".popUpWrapper");
+  //console.log(buttonsCharacter.length)
+  buttonsCharacter.forEach((element, index) => element.addEventListener("click", function () {
+    clickSelected(index)
+  }));
+
+  function clickSelected(index) {
+    wrapper.style.display = 'block';
+    document.querySelector(".name").textContent = dataSearch[index + start].name;
+    document.querySelector(".species").textContent = dataSearch[index + start].species;
+    document.querySelector(".origin").textContent = dataSearch[index + start].origin.name;
+    document.querySelector(".location").textContent = dataSearch[index + start].location.name;
+    document.querySelector(".image").src = dataSearch[index + start].image;
+  }
+
+  document.querySelector(".closePopUp").addEventListener("click", hidePopUp)
+
+  function hidePopUp() {
+    wrapper.style.display = 'none';
+  }
+}
+
+//funcion buscar personajes por letra
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".searchLetter").addEventListener("submit", validateLetter);
+});
+
+function validateLetter(event) {
+  start = 0;
+  end = 50;
+  event.preventDefault();
+  const letterSearch = document.querySelector(".az");
+  const letter = letterSearch.value.toUpperCase();
+  if (letter) {
+    dataSearch = data.results.filter((Characters) => Characters.name.split("")[0] === letter)
+    page()
+  }
+}
+
+//funcion ordenar de A-Z o Z-A
+document.querySelector(".organizarAZ").addEventListener("click", function () {
+  const way = 1;
+  ordenar(way)
+});
+
+document.querySelector(".organizarZA").addEventListener("click", function () {
+  const way = -1;
+  ordenar(way)
+});
+
+function ordenar(way) {
+  start = 0;
+  end = 50;
+  const NewArray = sortData(dataSearch, way);
+  dataSearch = NewArray;
+  page()
+}
+
+//filtrar por especies
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".Specie").addEventListener("change", filterSpecies);
+});
+
+function filterSpecies(event) {
+  start = 0;
+  end = 50;
+  event.preventDefault();
+  const Specie = document.querySelector(".Specie");
+  const SelectedSpecie = Specie.options[Specie.selectedIndex].value;
+  if (SelectedSpecie) {
+    dataSearch = filterData(data.results, SelectedSpecie);
+    page()
+  }
+}
+
+//funcion calcular
+document.querySelector(".calculate").addEventListener("click", calculate)
+
+function calculate (){
+  const wrapper = document.querySelector("#popUpWrapperCalculate");
+  wrapper.style.display = 'block';
+  const valueCalculate = computeStats(dataSearch);
+  document.querySelector(".Total").textContent = (dataSearch.length);
+  document.querySelector(".Alive").textContent = (+valueCalculate.Alive+ " %");
+  document.querySelector(".Dead").textContent = (+valueCalculate.Dead+ " %");
+  document.querySelector(".unknown").textContent = (+valueCalculate.unknown+ " %");
+  //console.log(valueCalculate)
+
+  document.querySelector("#ClosePopUpWrapperCalculate").addEventListener("click", hidePopUp)
+
+  function hidePopUp() {
+    wrapper.style.display = 'none';
+  }
+}
+
+//funcion paginacion botones mas y menos paginas
 document.querySelector(".more").addEventListener("click", more);
 
 function more() {
@@ -40,7 +145,6 @@ function more() {
   } else {
     cross = 0;
   }
-  console.log("entra mas");
   page()
 }
 document.querySelector(".less").addEventListener("click", less);
@@ -53,104 +157,5 @@ function less() {
   } else {
     cross = 0;
   }
-  console.log("entra menos");
   page()
 }
-
-function page() {
-  const container = document.querySelector('.container');
-  console.log("entra page");
-  container.innerHTML = dataSearch.slice(start, end).map((item) => `
-        <div class="card">
-        <button class="cardImage">
-            <img src="${item.image}" />
-        </button>
-        <h1 class="nameCard">${item.name}</h1>
-    </div>
-    `).join("");
-  console.log(start, end);
-  if (cross === 1) {
-    window.scrollTo(0, 0)
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".searchLetter").addEventListener("submit", validateLetter);
-});
-
-function validateLetter(event) {
-  start = 0;
-  end = 50;
-  event.preventDefault();
-  const letterSearch = document.querySelector(".az");
-  const letter = letterSearch.value.toUpperCase();
-  //console.log(letter)
-  if (letter) {
-    const rickandmorty = () => {
-      const container = document.querySelector('.container');
-      dataSearch = data.results.filter((Characters) => Characters.name.split("")[0] === letter)
-      console.log(dataSearch);
-      container.innerHTML = dataSearch.map((item) => `
-        <div class="card">
-        <button class="cardImage">
-            <img src="${item.image}" />
-        </button>
-        <h1 class="nameCard">${item.name}</h1>
-    </div>
-    `).join("");
-    }
-    rickandmorty()
-    page()
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".Specie").addEventListener("change", filterSpecies);
-});
-
-function filterSpecies(event) {
-  start = 0;
-  end = 50;
-  event.preventDefault();
-  const Specie = document.querySelector(".Specie");
-  const SelectedSpecie = Specie.options[Specie.selectedIndex].value;
-  //console.log(SelectedSpecie);
-
-  if (SelectedSpecie) {
-    const rickandmorty = () => {
-      if (SelectedSpecie === "Especies") {
-        completo()
-      } else {
-        const container = document.querySelector('.container');
-        dataSearch = data.results.filter((Character) => Character.species === SelectedSpecie)
-        container.innerHTML = dataSearch.map((item) => `
-        <div class="card">
-        <button class="cardImage">
-            <img src="${item.image}" />
-        </button>
-        <h1 class="nameCard">${item.name}</h1>
-    </div>
-    `).join("");
-      }
-    }
-    rickandmorty()
-    page()
-  }
-}
-/*
-const wrapper = document.querySelector(".popUpWrapper");
-
-document.querySelector(".cardImage").map((position) => position.addEventListener("click", popUp));
-
-function popUp () {
-    wrapper.style.display = 'block'; 
-    alert("Entró");
-    // console.log("entro");
-}
-
-document.querySelector(".closePopUp").addEventListener("click", closePopUp);
-
-function closePopUp () {
-    wrapper.style.display = 'none';   
-
-}*/
